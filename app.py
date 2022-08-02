19d49a7dd5253f1335c913b7f796d2583c2dbba3
Shalini Shekhar <shalini.ichha@gmail.com>
2022-08-01 21:48:29 -0400

    initial

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html') 
+
+@app.route('/google-charts/pie-chart')
+def google_pie_chart():
+    data = {'Task' : 'Hours per Day', 'Work' : 11, 'Eat' : 2, 'Commute' : 2, 'Watching TV' : 2, 'Sleeping' : 7}
+    return render_template('pie-chart.html', data=data)
+
+
+
+if __name__ == "__main__":
+    app.run()
